848a799c6c247c0f9761ba2c5240a57caaddb9a3
Djava <91669347+Javokhir91@users.noreply.github.com>
2024-04-14 23:11:44 +0300

    Add files via upload

---
+++ lesson_6/Program.java
@@ -0,0 +1,13 @@
+package ru.JDKitHW.lesson_6;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Paradox.withReplaceSelect(100);
+        System.out.println();
+        Paradox.NotReplaceSelect(100);
+
+    }
+
+}
